6823b0c3c6f4ad20eea9165fa55a72d5245c0380
Mitchell Roe <mbroe@oakland.edu>
2019-12-12 19:30:41 -0500

    Started working on a game character

---
+++ chapter12/12_2_game_character.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+import sys
+import pygame
+
+
+def main():
+    """Run the game"""
+
+    # Initialize game and create a screen object.
+    pygame.init()
+    pygame.display.set_caption("This is my pygame.")
+
+    while True:
+        # Watch for keyboard and mouse input.
+        for event in pygame.event.get():
+            if event.
+
+
+if __name__ == '__main__':
+    main()
