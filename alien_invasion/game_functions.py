d9d3934bbf93d03c7918f03be6e9cf0c99902138
Mitchell Roe <mbroe@oakland.edu>
2019-12-15 12:09:43 -0500

    Finished one more section of alien_invasion

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -6,21 +6,20 @@ import sys
 import pygame
 
 
-def check_events(ship):
-    """Respond to keypresses and mouse events."""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # For continuous movement
-                ship.moving_right = True
-                # Move the ship to the right
-                ship.rect.centerx += 1
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
 
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
@@ -31,3 +30,14 @@ def update_screen(ai_settings, screen, ship):
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
+
+def check_events(ship):
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
