a6b9c89140cf8570ebdb90a00af5824f791847f9
Mitchell Roe <mbroe@oakland.edu>
2019-12-15 12:10:05 -0500

    Finished exercise 12-3 Rocket

---
+++ chapter12/12_3_rocket/rocket.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+# rocket.py
+
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # Initialize pygame, settings, and a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+    ship = Ship(ai_settings, screen)
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+run_game()
