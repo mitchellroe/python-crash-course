26fcbe90e46d3c99242630669fba48a905d195c7
Mitchell Roe <mbroe@oakland.edu>
2019-12-21 11:07:34 -0500

    Added bullets

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@
 # alien_invasion.py
 
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -19,12 +20,22 @@ def run_game():
 
     # Make a ship
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        # print(len(bullets))
+
+        gf.update_screen(ai_settings, screen, ship, bullets)
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
